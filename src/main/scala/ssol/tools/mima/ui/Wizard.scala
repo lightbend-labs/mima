e2b01297400b3063693d77081d87b16148ba9c0c
Iulian Dragos <iulian.dragos@epfl.ch>
2011-01-17 11:27:54 +0100

    Some little steps towards adding the wizard class.

--- src/main/scala/ssol/tools/mima/ui/Wizard.scala
+++ src/main/scala/ssol/tools/mima/ui/Wizard.scala
@@ -2,11 +2,21 @@ package ssol.tools.mima.ui
 
 import scala.swing._
 
-class Wizard extends Panel {
+class Wizard extends BorderPanel {
+	import BorderPanel._
+	
 	val buttonsPanel = new BoxPanel(Orientation.Horizontal)
 	
 	val backButton = Button("Back") { 
 	}
+	val nextButton = new Button("Next")
+	val exitButton = new Button("Exit")
 	
-	buttonsPanel += backButton
+	val centerPane = new FlowPanel {
+		preferredSize = new Dimension(300, 300)
+	}
+	
+	buttonsPanel.contents += (backButton, nextButton, exitButton)
+	add(centerPane, Position.Center)
+	add(buttonsPanel, Position.South)
 }
