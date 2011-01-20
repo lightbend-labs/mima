4d55ae997c74c003d987a0131afdf11f4c18ff12
Iulian Dragos <iulian.dragos@epfl.ch>
2011-01-20 11:43:32 +0100

    Fixed sbt build. Added dependency on scala-swing and moved to scala 2.8.1.

--- project/build/MimaProject.scala
+++ project/build/MimaProject.scala
@@ -5,6 +5,8 @@ class MimaProject(info: ProjectInfo) extends DefaultProject(info) {
   // Add Maven Local repository for SBT to search for (disable if this doesn't suit you)
   val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
 
+  val scalaSwing = "org.scala-lang" % "scala-swing" % "2.8.1"
+
   override def managedStyle = ManagedStyle.Maven
   
 }
