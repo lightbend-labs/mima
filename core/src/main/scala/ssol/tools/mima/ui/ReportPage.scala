fa19d4032024158dcb0b4451dccda6c89b44a94b
Mirco <mirco@Mircos-MacBook-Pro.local>
2011-04-18 23:29:00 +0200

    update table to render a checkbox in column fix status

--- core/src/main/scala/ssol/tools/mima/ui/ReportPage.scala
+++ core/src/main/scala/ssol/tools/mima/ui/ReportPage.scala
@@ -17,25 +17,8 @@ import java.awt.GridBagConstraints._
  * Mima problem report page.
  */
 class ReportPage extends GridBagPanel with WithConstraints with wizard.WizardPanel {
-  import Config.info
 
-  private val errorLabel = new Label("Unfortunately there are some unfixable incompatibilities.") {
-    foreground = java.awt.Color.RED
-  }
-  
-  val defaultFilterText = "<enter filter>"  
-    
-  val ins = new Insets(0, 10, 10, 10)
-  
-  withConstraints(insets = ins, gridx = 0, gridy = RELATIVE, anchor = Anchor.West) {
-    add(errorLabel, _)
-  }
-  withConstraints(insets = ins, gridx = 0, fill = Fill.Both)(add(new Separator, _))
-  val filter = new TextField(defaultFilterText)
-  withConstraints(insets = ins, gridx = 0, fill = Fill.Horizontal)(add(filter, _))
-
-  // the problem table
-  val table = new JTable() {
+  class ReportTable extends JTable with javax.swing.event.TableModelListener {
     override def prepareRenderer(renderer: TableCellRenderer, row: Int, column: Int): java.awt.Component = {
       val component = super.prepareRenderer(renderer, row, column)
       component match {
@@ -46,7 +29,37 @@ class ReportPage extends GridBagPanel with WithConstraints with wizard.WizardPan
       }
       component
     }
+
+    override def getCellRenderer(row: Int, column: Int): TableCellRenderer = {
+      if (getModel.getValueAt(row, column).isInstanceOf[Boolean] && !getModel.isCellEditable(row, column))
+        LabelCellRenderer
+      else
+        super.getCellRenderer(row, column)
+    }
+
   }
+
+  object LabelCellRenderer extends DefaultTableCellRenderer
+
+  import Config.info
+
+  private val errorLabel = new Label("Unfortunately there are some unfixable incompatibilities.") {
+    foreground = java.awt.Color.RED
+  }
+
+  val defaultFilterText = "<enter filter>"
+
+  val ins = new Insets(0, 10, 10, 10)
+
+  withConstraints(insets = ins, gridx = 0, gridy = RELATIVE, anchor = Anchor.West) {
+    add(errorLabel, _)
+  }
+  withConstraints(insets = ins, gridx = 0, fill = Fill.Both)(add(new Separator, _))
+  val filter = new TextField(defaultFilterText)
+  withConstraints(insets = ins, gridx = 0, fill = Fill.Horizontal)(add(filter, _))
+
+  // the problem table
+  val table = new ReportTable()
 
   withConstraints(gridx = 0, fill = Fill.Both, weighty = 1.0, weightx = 1.0, gridwidth = REMAINDER, insets = ins) {
     add(new ScrollPane(new Component {
@@ -54,12 +67,12 @@ class ReportPage extends GridBagPanel with WithConstraints with wizard.WizardPan
     }), _)
   }
 
-  var sorter: TableRowSorter[ProblemsModel] = _
+  var sorter: TableRowSorter[AbstractTableModel] = _
 
   def doCompare(oldDir: String, newDir: String, mimalib: MiMaLib) {
     info("old: " + oldDir + " new: " + newDir)
     val problems = mimalib.collectProblems(oldDir, newDir)
-    val model = new ProblemsModel(problems)
+    val model = ProblemsModel(problems)
     errorLabel.visible = model.hasUnfixableProblems
     table.setModel(model)
     table.getColumnModel.getColumn(0).setPreferredWidth(70)
@@ -76,7 +89,7 @@ class ReportPage extends GridBagPanel with WithConstraints with wizard.WizardPan
   reactions += {
     case ValueChanged(`filter`) =>
       try {
-        val rf: RowFilter[ProblemsModel, Integer] = RowFilter.regexFilter(escape(filter.text), 1, 2)
+        val rf: RowFilter[AbstractTableModel, Integer] = RowFilter.regexFilter(escape(filter.text), 1, 2)
         sorter.setRowFilter(rf)
       } catch {
         case _ => () // swallow any illegal regular expressions
@@ -84,52 +97,65 @@ class ReportPage extends GridBagPanel with WithConstraints with wizard.WizardPan
     case FocusGained(`filter`, _, _) if (filter.text == defaultFilterText) =>
       filter.text = ""
   }
-  
 
   private def escape(str: String): String = {
     str flatMap {
       case '$' => "\\$"
       case '+' => "\\+"
-      case c   => c.toString
+      case c => c.toString
     }
   }
 }
 
-class ProblemsModel(problems: List[Problem]) extends AbstractTableModel {
-  val columns = Array("Status", "Member", "Description", "Fix?")
+object ProblemsModel {
 
-  import scala.collection.JavaConversions._
-  import java.util.Vector
+  def apply(problems: List[Problem]) =
+    new ProblemsModel(problems.toArray.map(toArray(_)))
 
-  lazy val hasUnfixableProblems = problems.exists(_.status == Problem.Status.Unfixable)
-  
-  override def getColumnName(n: Int) = columns(n)
-
-  override def getColumnCount = columns.size
-  override def getRowCount = problems.size
-  override def getValueAt(x: Int, y: Int) = {
-    val p = problems(x)
-    y match {
-      case 0 => p.status
-      case 1 => getReferredMember(p)
-      case 2 => p.description
-      case 3 => false.asInstanceOf[AnyRef]
-    }
-  }
-
-  override def isCellEditable(i: Int, j: Int) = j == 3
+  private def toArray(problem: Problem): Array[AnyRef] =
+    Array(problem.status, getReferredMember(problem), problem.description, false.asInstanceOf[AnyRef])
 
   private def getReferredMember(p: Problem): String = p match {
-    case MissingFieldProblem(oldfld)                   => oldfld.fullName
-    case MissingMethodProblem(oldmth)                  => oldmth.fullName
-    case MissingClassProblem(oldclz)                   => oldclz.toString
-    case MissingPackageProblem(oldpkg)                 => oldpkg.toString
-    case InaccessibleFieldProblem(newfld)              => newfld.fullName
-    case InaccessibleMethodProblem(newmth)             => newmth.fullName
-    case InaccessibleClassProblem(newcls)              => newcls.toString
-    case IncompatibleFieldTypeProblem(oldfld, newfld)  => oldfld.fullName
-    case IncompatibleMethTypeProblem(oldmth, newmth)   => oldmth.fullName
+    case MissingFieldProblem(oldfld) => oldfld.fullName
+    case MissingMethodProblem(oldmth) => oldmth.fullName
+    case MissingClassProblem(oldclz) => oldclz.toString
+    case MissingPackageProblem(oldpkg) => oldpkg.toString
+    case InaccessibleFieldProblem(newfld) => newfld.fullName
+    case InaccessibleMethodProblem(newmth) => newmth.fullName
+    case InaccessibleClassProblem(newcls) => newcls.toString
+    case IncompatibleFieldTypeProblem(oldfld, newfld) => oldfld.fullName
+    case IncompatibleMethTypeProblem(oldmth, newmth) => oldmth.fullName
     case IncompatibleResultTypeProblem(oldmth, newmth) => oldmth.fullName
-    case AbstractMethodProblem(oldmeth)                => oldmeth.fullName
+    case AbstractMethodProblem(oldmeth) => oldmeth.fullName
+  }
+
+  class ProblemsModel(problems: Array[Array[AnyRef]]) extends AbstractTableModel {
+    val columns = Array("Status", "Member", "Description", "Fix?")
+
+    import scala.collection.JavaConversions._
+    import java.util.Vector
+
+    def hasUnfixableProblems = problems.exists(_(0) == Problem.Status.Unfixable)
+
+    def isFixableProblem(row: Array[AnyRef]) = row(0) != Problem.Status.Unfixable
+
+    override def getColumnName(n: Int) = columns(n)
+
+    override def getColumnCount = columns.size
+    override def getRowCount = problems.size
+    override def getValueAt(x: Int, y: Int): AnyRef = problems(x)(y)
+
+    override def isCellEditable(i: Int, j: Int) =
+      j == 3 && isFixableProblem(problems(i))
+
+    override def getColumnClass(col: Int) = {
+      getValueAt(0, col).getClass()
+    }
+
+    override def setValueAt(value: AnyRef, row: Int, col: Int) = {
+      problems(row)(col) = value
+      fireTableCellUpdated(row, col);
+    }
   }
 }
+
