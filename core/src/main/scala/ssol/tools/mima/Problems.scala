ec2c23d5df289c9b0730554806afd4f0005dc67a
Mirco <mirco@Mircos-iMac.local>
2011-05-03 11:06:53 +0200

    refactoring on the problem classes

--- core/src/main/scala/ssol/tools/mima/Problems.scala
+++ core/src/main/scala/ssol/tools/mima/Problems.scala
@@ -3,57 +3,72 @@ package ssol.tools.mima
 object Problem {
   object Status extends Enumeration {
     val Unfixable = Value("unfixable")
-    val Upgradable = Value("upgradable") // means mima client can fix the bytecode
-    val SourceFixable = Value("source fixable") // means that the break can be easily fixed in the source
+    val Upgradable = Value("upgradable") // means MiMa Client can fix the bytecode
+    val SourceFixable = Value("source fixable") // means that the break may be easily fixed in the source
     val Ignored = Value("ignored")
   }
 }
 
-sealed abstract class Problem(val description: String, initialStatus: Problem.Status.Value = Problem.Status.Unfixable) {
-  var status: Problem.Status.Value = initialStatus
+sealed abstract class Problem(val status: Problem.Status.Value = Problem.Status.Unfixable) {
+  val description: String
 }
 
-case class MissingFieldProblem(oldfld: MemberInfo) extends
-  Problem(oldfld.fieldString+" does not have a correspondent in new version")
+case class MissingFieldProblem(oldfld: MemberInfo) extends Problem {
+  override val description = oldfld.fieldString + " does not have a correspondent in new version"
+}
 
-case class MissingMethodProblem(oldmeth: MemberInfo) extends
-  Problem(oldmeth.methodString+" does not have a correspondent in new version",
-      if(oldmeth.owner.isTrait) Problem.Status.Upgradable else Problem.Status.Unfixable)
+case class MissingMethodProblem(oldmeth: MemberInfo) extends Problem(if (oldmeth.owner.isTrait) Problem.Status.Upgradable else Problem.Status.Unfixable) {
+  override val description = oldmeth.methodString + " does not have a correspondent in new version"
+}
 
-case class MissingClassProblem(oldclazz: ClassInfo) extends
-  Problem(oldclazz.classString+" does not have a correspondent in new version")
+case class MissingClassProblem(oldclazz: ClassInfo) extends Problem {
+  override val description = oldclazz.classString + " does not have a correspondent in new version"
+}
 
-case class MissingPackageProblem(oldpkg: PackageInfo) extends
-  Problem(oldpkg.packageString+" does not have a correspondent in new version")
+case class MissingPackageProblem(oldpkg: PackageInfo) extends Problem {
+  override val description = oldpkg.packageString + " does not have a correspondent in new version"
+}
 
-case class InaccessibleFieldProblem(newfld: MemberInfo) extends
-  Problem(newfld.fieldString+" was public; is inaccessible in new version")
+case class InaccessibleFieldProblem(newfld: MemberInfo) extends Problem {
+  override val description = newfld.fieldString + " was public; is inaccessible in new version"
+}
 
-case class InaccessibleMethodProblem(newmeth: MemberInfo) extends
-  Problem(newmeth.methodString+" was public; is inaccessible in new version")
+case class InaccessibleMethodProblem(newmeth: MemberInfo) extends Problem {
+  override val description = newmeth.methodString + " was public; is inaccessible in new version"
+}
 
-case class InaccessibleClassProblem(newclazz: ClassInfo) extends
-  Problem(newclazz.classString+" was public; is inaccessible in new version")
+case class InaccessibleClassProblem(newclazz: ClassInfo) extends Problem {
+  override val description = newclazz.classString + " was public; is inaccessible in new version"
+}
 
-case class IncompatibleFieldTypeProblem(oldfld: MemberInfo, newfld: MemberInfo) extends
-  Problem(newfld.fieldString+"'s type has changed; was: "+oldfld.tpe+", is now: "+newfld.tpe)
+case class IncompatibleFieldTypeProblem(oldfld: MemberInfo, newfld: MemberInfo) extends Problem {
+  override val description = newfld.fieldString + "'s type has changed; was: " + oldfld.tpe + ", is now: " + newfld.tpe
+}
 
-case class IncompatibleMethTypeProblem(oldmeth: MemberInfo, newmeths: List[MemberInfo])(implicit status: Problem.Status.Value = Problem.Status.SourceFixable) extends
-  Problem(
-    oldmeth.methodString+(
-    if (newmeths.tail.isEmpty)
-      "'s type has changed; was "+oldmeth.tpe+", is now: "+newmeths.head.tpe
+case class IncompatibleMethTypeProblem(oldmeth: MemberInfo, newmeths: List[MemberInfo])(implicit status: Problem.Status.Value = Problem.Status.SourceFixable) extends Problem(status) {
+  override val description = {
+    oldmeth.methodString + (if (newmeths.tail.isEmpty)
+      "'s type has changed; was " + oldmeth.tpe + ", is now: " + newmeths.head.tpe
     else
-      " does not have a correspondent with same parameter signature among "+
-      (newmeths map (_.tpe) mkString ", ")), status)
+      " does not have a correspondent with same parameter signature among " +
+        (newmeths map (_.tpe) mkString ", "))
+  }
+}
 
-case class IncompatibleResultTypeProblem(oldmeth: MemberInfo, newmeth: MemberInfo)(implicit status: Problem.Status.Value = Problem.Status.SourceFixable) extends
-  Problem(oldmeth.methodString+" has now a different result type; was: "+
-          oldmeth.tpe.resultType+", is now: "+newmeth.tpe.resultType,  status)
+case class IncompatibleResultTypeProblem(oldmeth: MemberInfo, newmeth: MemberInfo)(implicit status: Problem.Status.Value = Problem.Status.SourceFixable) extends Problem(status) {
+  override val description = {
+    oldmeth.methodString + " has now a different result type; was: " +
+      oldmeth.tpe.resultType + ", is now: " + newmeth.tpe.resultType
+  }
+}
 
-case class AbstractMethodProblem(newmeth: MemberInfo) extends
-  Problem("abstract "+newmeth.methodString+" does not have a correspondent in old version", Problem.Status.Upgradable)
+case class AbstractMethodProblem(newmeth: MemberInfo) extends Problem(Problem.Status.Upgradable) {
+  override val description = "abstract " + newmeth.methodString + " does not have a correspondent in old version"
+}
 
-case class IncompatibleClassDeclarationProblem(oldClazz: ClassInfo, newClazz: ClassInfo) extends
-  Problem("declaration of " + oldClazz.description + " has changed to " + newClazz.description +
-      " in new version; changing " + oldClazz.declarationPrefix +" to " + newClazz.declarationPrefix + " breaks client code")
+case class IncompatibleClassDeclarationProblem(oldClazz: ClassInfo, newClazz: ClassInfo) extends Problem {
+  override val description = {
+    "declaration of " + oldClazz.description + " has changed to " + newClazz.description +
+      " in new version; changing " + oldClazz.declarationPrefix + " to " + newClazz.declarationPrefix + " breaks client code"
+  }
+}
