109202fd4cb5837b05cadef5a034f0a617e16640
Mirco <mirco@Mircos-MacBook-Pro.local>
2011-05-23 17:15:37 +0200

    refactored ReportTable

--- core/src/main/scala/ssol/tools/mima/core/ui/widget/ReportTable.scala
+++ core/src/main/scala/ssol/tools/mima/core/ui/widget/ReportTable.scala
@@ -6,20 +6,28 @@ import javax.swing.event.TableModelListener
 import javax.swing.ListSelectionModel
 
 private[ui] class ReportTable extends JTable with TableModelListener {
-  // it makes sense to allow only single selection
+  // allow only single selection
   setSelectionMode(ListSelectionModel.SINGLE_SELECTION)
-
-  //overriden to set tooltip
+  
   override def prepareRenderer(renderer: TableCellRenderer, row: Int, column: Int): java.awt.Component = {
     val component = super.prepareRenderer(renderer, row, column)
 
     component match {
       case jc: javax.swing.JComponent =>
-        /** Display a tooltip*/
-        jc.setToolTipText(getValueAt(row, column).toString.grouped(80).mkString("<html>", "<br>", "</html>"))
+        /** Display a tooltip over each table's element*/
+        val element = getValueAt(row, column)
+        val html = htmlFor(element)
+        jc.setToolTipText(html)
       case _ => ()
     }
 
     component
+  }
+  
+  private def htmlFor(value: Any): String = {
+    if(value == null)
+      ""
+    else
+      value.toString.grouped(80).mkString("<html>", "<br>", "</html>")
   }
 }
