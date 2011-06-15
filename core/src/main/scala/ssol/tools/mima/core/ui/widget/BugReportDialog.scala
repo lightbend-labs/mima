660f7f427aabe43d22a0cc8005ff621ce4a9db8b
Mirco <mirco@Mircos-iMac.local>
2011-06-15 16:56:30 +0200

    more comments on the bug report dialog

--- core/src/main/scala/ssol/tools/mima/core/ui/widget/BugReportDialog.scala
+++ core/src/main/scala/ssol/tools/mima/core/ui/widget/BugReportDialog.scala
@@ -10,20 +10,21 @@ import ssol.tools.mima.core.util.Urls
 import javax.swing.BorderFactory
 import javax.swing.border.TitledBorder
 
-/** 
- * Simple dialog for reporting unexpected failures.
- * The dialog contains
+/** Simple dialog for reporting unexpected failures.
+ *  The dialog is `Centered` in the screen.
+ *  The dialog contains
  *    (1) An hyperlink to the bug reporting site
  *    (2) A textarea containing the exception's stack trace
  *    (3) A `continue` button to close the dialog
- *    
- * @precondition: the `error` must not be null
+ *
+ *  @precondition: the `error` must not be null
  */
 class BugReportDialog(error: Throwable, owner: Window = null) extends Dialog(owner) with Centered {
   assert(error != null)
 
   title = "Unexpected Error"
 
+  /** Short message explaining the user what he should do to report the error.*/
   private val explanation = new HtmlViewPane {
     setHtml("An unexpected error occurred. Please create a " +
       "<a href=" + Urls.BugReportingSite + ">new ticket</a> describing " +
@@ -41,13 +42,20 @@ class BugReportDialog(error: Throwable, owner: Window = null) extends Dialog(own
       text = error.getStackTrace.mkString("\n")
       wordWrap = true
       lineWrap = true
-      rows = 10
+      rows = 10 // make sure the dialog size is fixed!
     }
     override lazy val peer = stackTraceContent.peer
   })
 
+  /** The continue button is used to dispose the dialog */
   private val continue = new Button("Continue")
 
+  listenTo(continue)
+  reactions += {
+    case ButtonClicked(`continue`) => dispose()
+  }
+
+  /** lay out all UI elements vertically */
   contents = new BoxPanel(Orientation.Vertical) {
     border = EmptyBorder(10)
 
@@ -61,17 +69,10 @@ class BugReportDialog(error: Throwable, owner: Window = null) extends Dialog(own
     contents += new FlowPanel(FlowPanel.Alignment.Center)(continue) { vGap = 0; hGap = 0 }
   }
 
-  listenTo(continue)
-  reactions += {
-    case ButtonClicked(`continue`) => dispose()
-  }
-
   location = center
   modal = true
-  resizable = false
-
+  resizable = false 
   pack()
-
   visible = true
 }
   
