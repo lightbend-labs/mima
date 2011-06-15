3a9e5783361e599b0883e835e4e8eef1b1ec1464
Mirco <mirco@Mircos-iMac.local>
2011-06-15 16:42:54 +0200

    updated version

--- core/src/main/scala/ssol/tools/mima/core/util/Version.scala
+++ core/src/main/scala/ssol/tools/mima/core/util/Version.scala
@@ -1,6 +1,6 @@
 package ssol.tools.mima.core.util
 
 object Version {
-  // FIXME: How can I inject the version number?
-  val version = "v.0.1" 
+  // FIXME: I need a plugin that can filter resources...
+  val version = "v.0.1.1" 
 }
