89155f9e90932ae545b8bf86a9082ca9b5ca54fa
Mirco Dotta <mirco.dotta@typesafe.com>
2011-07-04 21:29:18 +0200

    Make sure to display the closing dialog also when the user attempts to close the frame by clicking on the frame close icon

--- core/src/main/scala/ssol/tools/mima/core/ui/MimaFrame.scala
+++ core/src/main/scala/ssol/tools/mima/core/ui/MimaFrame.scala
@@ -12,10 +12,11 @@ import scala.tools.nsc.{ util, io }
 import util._
 import ClassPath._
 
-abstract class MimaFrame extends MainFrame with Centered {  
+abstract class MimaFrame extends MainFrame with Centered {
   import ssol.tools.mima.core.util.Urls._
+
   object ScalaLogo extends widget.LinkImagePanel(ScalaSite, images.Icons.scalaLogo)
-  
+
   object TypesafeLogo extends widget.LinkImagePanel(TypesafeSite, images.Icons.typesafe)
 
   title = "Migration Manager - " + Version.version
@@ -24,7 +25,6 @@ abstract class MimaFrame extends MainFrame with Centered {
   location = center
   resizable = false
 
-  
   protected val mainContainer = new BorderPanel {
     border = EmptyBorder(10)
 
@@ -42,22 +42,28 @@ abstract class MimaFrame extends MainFrame with Centered {
     }
   }
 
-  
+  override def closeOperation() { showCloseDialog() }
+
   menuBar = MimaMenuBar
-  
+
   contents = mainContainer
-  
+
+  peer.setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE)
+
   listenTo(menuBar)
-  
+
   reactions += {
-    case ExitMiMa =>
-      Dialog.showConfirmation(parent = null,
-        icon = images.Icons.exit,
-        title = "Exit MiMa",
-        message = "Are you sure you want to quit?") match {
-          case Dialog.Result.Ok => exit(0)
-          case _                => ()
-        }
+    case ExitMiMa => showCloseDialog()
+  }
+
+  private def showCloseDialog() {
+    Dialog.showConfirmation(parent = null,
+      icon = images.Icons.exit,
+      title = "Exit MiMa",
+      message = "Are you sure you want to quit?") match {
+        case Dialog.Result.Ok => exit(0)
+        case _ => ()
+      }
   }
 
   protected def startWizard(wizard: Wizard) {
