c7192914cb1fad008c16984abeda132cf8b0d0f9
Josh Suereth <joshua.suereth@gmail.com>
2012-03-14 23:32:41 -0400

    Added a main to run MIMA via the command line.   Still need to pretty-print results.

---
+++ reporter/src/main/scala/ssol/tools/mima/cli/Main.scala
@@ -0,0 +1,76 @@
+package ssol.tools.mima
+package cli
+
+import lib.MiMaLib
+import scala.tools.cmd._
+import scala.tools.nsc.util.{JavaClassPath,ClassPath}
+import ClassPath.DefaultJavaContext
+import core.Config
+
+/** A program to run the MIMA tools from the command line.
+ */
+trait MimaSpec extends Spec with Meta.StdOpts with Interpolation {
+  lazy val referenceSpec  = MimaSpec
+  lazy val programInfo    = Spec.Info("demo", "Usage: mima [<options>]", "ssol.tools.mima.Main")
+
+  help("""Usage: mima [<options>]""")
+  heading("necessary options:")
+  val prevfile = "prev" / "Previous classpath/jar for binary compatibility testing." defaultTo ""
+  val currentfile = "curr" / "Current classpath/jar for binary compatibility testing." defaultTo ""
+  heading("optional settings:")
+  val classpath    = "classpath"    / "an optional classpath setting"        defaultTo System.getProperty("java.class.path")
+}
+
+object MimaSpec extends MimaSpec with Property {
+  lazy val propMapper = new PropertyMapper(MimaSpec)
+
+  type ThisCommandLine = SpecCommandLine
+  def creator(args: List[String]) =
+    new SpecCommandLine(args) {
+      override def errorFn(msg: String) = { println("Error: " + msg) ; sys.exit(0) }
+    }
+}
+
+/** Helper method for Mima Reporter runner. */
+class Main(args: List[String]) extends {
+  val parsed = MimaSpec(args: _*)
+} with MimaSpec with Instance {
+
+  def helpMsg = MimaSpec.helpMsg
+  
+  val (curr, prev) =
+    (currentfile, prevfile) match {
+      case ("", "")   if residualArgs.size == 2 => (residualArgs.head, residualArgs.tail.head)
+      case ("", last) if residualArgs.size == 1 => (residualArgs.head, last)
+      case (head, _)  if residualArgs.size == 1 => (head, residualArgs.head)
+      case other                                => other
+    }
+  
+  def isDefined: Boolean = !(curr.isEmpty || prev.isEmpty)
+
+  def makeClasspath = new JavaClassPath(
+      // TODO expand path?
+      DefaultJavaContext.classesInPath(classpath).toIndexedSeq, DefaultJavaContext)
+  
+  def makeMima = {
+    // TODO: get mima to use paul's CMD library *or* 
+    // hack from one to the other temporarily.
+    Config.setup("mima", Array.empty)
+    new MiMaLib(makeClasspath)
+  }
+  
+  def run(): Unit = {
+    val mima = makeMima
+    val problems = mima.collectProblems(prevfile, currentfile)
+    problems foreach println
+  }
+}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val runner = new Main(args.toList)
+    
+    if (args.isEmpty || !runner.isDefined) println(runner.helpMsg)
+    else                                  runner.run()
+  }
+}
