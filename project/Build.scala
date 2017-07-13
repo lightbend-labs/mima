d5ee149ee5e97d869e3919b929ab2dee623efd17
Martynas Mickevičius <martynas@2m.lt>
2017-07-13 12:45:04 +0300

    Keep dependency of scripted tasks

--- project/Build.scala
+++ project/Build.scala
@@ -149,6 +149,49 @@ object MimaBuild {
              scriptedBufferLog := false,
              // Scripted locally publishes sbt plugin and then runs test projects with locally published version.
              // Therefore we also need to locally publish dependent projects on scripted test run.
+             scripted := {
+              val args = ScriptedPlugin.asInstanceOf[{
+                def scriptedParser(f: File): complete.Parser[Seq[String]]
+              }].scriptedParser(sbtTestDirectory.value).parsed
+              val prereq: Unit = scriptedDependencies.value
+              try {
+                if((sbtVersion in pluginCrossBuild).value == "1.0.0-M6") {
+                  ScriptedPlugin.scriptedTests.value.asInstanceOf[{
+                    def run(
+                      x1: File,
+                      x2: Boolean,
+                      x3: Array[String],
+                      x4: File,
+                      x5: Array[String],
+                      x6: java.util.List[File]
+                    ): Unit
+                  }].run(
+                    sbtTestDirectory.value,
+                    scriptedBufferLog.value,
+                    args.toArray,
+                    sbtLauncher.value,
+                    scriptedLaunchOpts.value.toArray,
+                    new java.util.ArrayList()
+                  )
+                } else {
+                  ScriptedPlugin.scriptedTests.value.asInstanceOf[{
+                    def run(
+                      x1: File,
+                      x2: Boolean,
+                      x3: Array[String],
+                      x4: File,
+                      x5: Array[String]
+                    ): Unit
+                  }].run(
+                    sbtTestDirectory.value,
+                    scriptedBufferLog.value,
+                    args.toArray,
+                    sbtLauncher.value,
+                    scriptedLaunchOpts.value.toArray
+                  )
+                }
+              } catch { case e: java.lang.reflect.InvocationTargetException => throw e.getCause }
+             },
              scripted := (scripted dependsOn (publishLocal in core, publishLocal in reporter)).evaluated)
     dependsOn(reporter)
     settings(sbtPublishSettings:_*)
