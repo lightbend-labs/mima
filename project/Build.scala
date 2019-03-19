0b754fa376d6d27f8ab6d62c25e4042aded4af0c
Scala steward <me@scala-steward.org>
2019-03-19 10:20:45 +0100

    Update scalatest to 3.0.7

--- project/Build.scala
+++ project/Build.scala
@@ -83,7 +83,7 @@ object BuildSettings {
 
 object Dependencies {
   val typesafeConfig = "com.typesafe" % "config" % "1.3.3"
-  val scalatest = "org.scalatest" %% "scalatest" % "3.0.6" % Test
+  val scalatest = "org.scalatest" %% "scalatest" % "3.0.7" % Test
 
 }
 
