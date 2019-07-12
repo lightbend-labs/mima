dc3f8412f99f16c01b55b34b61128b5123e91a83
Dale Wijnand <dale.wijnand@gmail.com>
2019-07-12 19:07:29 +0100

    Sort & re-split MimaKeys, according to importance/usage

--- sbtplugin/src/main/scala/com/typesafe/tools/mima/plugin/MimaKeys.scala
+++ sbtplugin/src/main/scala/com/typesafe/tools/mima/plugin/MimaKeys.scala
@@ -1,26 +1,25 @@
 package com.typesafe.tools.mima
 package plugin
 
+import com.typesafe.tools.mima.core.{ Problem, ProblemFilter }
 import sbt._
 
 object MimaKeys extends MimaKeys
 
 class MimaKeys {
 
-  final val mimaFailOnProblem      = settingKey[Boolean]("if true, fail the build on binary incompatibility detection.")
-  final val mimaFailOnNoPrevious   = settingKey[Boolean]("if true, fail the build if no previous artifacts are set.")
-  final val mimaPreviousArtifacts  = settingKey[Set[ModuleID]]("Previous released artifacts used to test binary compatibility.")
-  final val mimaPreviousClassfiles = taskKey[Map[ModuleID, File]]("Directories or jars containing the previous class files used to test compatibility with a given module.")
-  final val mimaCurrentClassfiles  = taskKey[File]("Directory or jar containing the current class files used to test compatibility.")
-  final val mimaFindBinaryIssues   = taskKey[Map[ModuleID, (List[core.Problem], List[core.Problem])]]("All backward and forward binary incompatibilities between a given module and current project.")
-  final val mimaReportBinaryIssues = taskKey[Unit]("Logs all binary incompatibilities to the sbt console/logs.")
+  final val mimaPreviousArtifacts    = settingKey[Set[ModuleID]]("Previous released artifacts used to test binary compatibility.")
+  final val mimaReportBinaryIssues   = taskKey[Unit]("Logs all binary incompatibilities to the sbt console/logs.")
+  final val mimaBinaryIssueFilters   = taskKey[Seq[ProblemFilter]]("Filters to apply to binary issues found. Applies both to backward and forward binary compatibility checking.")
+  final val mimaFailOnProblem        = settingKey[Boolean]("if true, fail the build on binary incompatibility detection.")
+  final val mimaFailOnNoPrevious     = settingKey[Boolean]("if true, fail the build if no previous artifacts are set.")
 
-  final val mimaFiltersDirectory = settingKey[File]("Directory containing issue filters.")
-
-  final val mimaBinaryIssueFilters   = taskKey[Seq[core.ProblemFilter]]("Filters to apply to binary issues found. Applies both to backward and forward binary compatibility checking.")
-  final val mimaBackwardIssueFilters = taskKey[Map[String, Seq[core.ProblemFilter]]]("Filters to apply to binary issues found grouped by version of a module checked against. These filters only apply to backward compatibility checking.")
-  final val mimaForwardIssueFilters  = taskKey[Map[String, Seq[core.ProblemFilter]]]("Filters to apply to binary issues found grouped by version of a module checked against. These filters only apply to forward compatibility checking.")
-
-  final val mimaCheckDirection = settingKey[String]("Compatibility checking direction; default is \"backward\", but can also be \"forward\" or \"both\".")
+  final val mimaPreviousClassfiles   = taskKey[Map[ModuleID, File]]("Directories or jars containing the previous class files used to test compatibility with a given module.")
+  final val mimaCurrentClassfiles    = taskKey[File]("Directory or jar containing the current class files used to test compatibility.")
+  final val mimaCheckDirection       = settingKey[String]("Compatibility checking direction; default is \"backward\", but can also be \"forward\" or \"both\".")
+  final val mimaFindBinaryIssues     = taskKey[Map[ModuleID, (List[Problem], List[Problem])]]("All backward and forward binary incompatibilities between a given module and current project.")
+  final val mimaBackwardIssueFilters = taskKey[Map[String, Seq[ProblemFilter]]]("Filters to apply to binary issues found grouped by version of a module checked against. These filters only apply to backward compatibility checking.")
+  final val mimaForwardIssueFilters  = taskKey[Map[String, Seq[ProblemFilter]]]("Filters to apply to binary issues found grouped by version of a module checked against. These filters only apply to forward compatibility checking.")
+  final val mimaFiltersDirectory     = settingKey[File]("Directory containing issue filters.")
 
 }
