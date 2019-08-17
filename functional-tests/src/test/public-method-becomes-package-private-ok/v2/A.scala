0468c9e7a99c8fe19f23cdc4c2976232ef5279fd
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2019-08-17 17:47:54 -0400

    Add test for public method becomes private[lib]

---
+++ functional-tests/src/test/public-method-becomes-package-private-ok/v2/A.scala
@@ -0,0 +1,8 @@
+package bar
+class A[T](t: T)
+object A {
+  def foo[T](implicit x: T): A[T] = ???
+  private[bar] def foo[T](implicit x: T, y: T): A[T] = ???
+  def foo[T](f: T => T): A[T] = ???
+  def foo[T](f: (T, T) => T): A[T] = ???
+}
