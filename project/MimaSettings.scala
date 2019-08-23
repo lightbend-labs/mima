be31360a62937cfe004ab08828ed326057cdcf54
Arnout Engelen <arnout@bzzt.net>
2019-08-23 09:09:28 +0200

    Update sbt-mima-plugin to 0.6.0

--- project/MimaSettings.scala
+++ project/MimaSettings.scala
@@ -10,7 +10,7 @@ import com.typesafe.tools.mima.plugin.MimaPlugin.autoImport._
 
 object MimaSettings {
   // clear out mimaBinaryIssueFilters when changing this
-  val mimaPreviousVersion = "0.5.0"
+  val mimaPreviousVersion = "0.6.0"
 
   private val isScala213OrLater =
     Def.setting(VersionNumber(scalaVersion.value).matchesSemVer(SemanticSelector(">=2.13")))
@@ -38,50 +38,6 @@ object MimaSettings {
       // * com.typesafe.tools.mima.core.Config.setup
       // * com.typesafe.tools.mima.core.reporterClassPath
       // * com.typesafe.tools.mima.lib.MiMaLib.collectProblems
-
-      // Through GitHub search this looks totally unused
-      // Dropped to split the settings into global/build/projectSettings
-      ProblemFilters.exclude[DirectMissingMethodProblem]("*mima.plugin.MimaPlugin.mimaReportSettings"),
-
-      // Dropped deprecated method
-      ProblemFilters.exclude[DirectMissingMethodProblem]("*mima.core.ProblemFilters.excludePackage"),
-      // Changes to the members of the *Problem classes
-      ProblemFilters.exclude[MemberProblem]("*mima.core.*Problem.*"),
-      // *Problem classes made final
-      ProblemFilters.exclude[FinalClassProblem]("*mima.core.*Problem"),
-      // Dropped unused methods on classes in the hierarchy of the *Problem classes
-      ProblemFilters.exclude[DirectMissingMethodProblem]("*mima.core.ProblemRef.*"),
-      ProblemFilters.exclude[DirectMissingMethodProblem]("*mima.core.TemplateRef.*"),
-      ProblemFilters.exclude[DirectMissingMethodProblem]("*mima.core.MemberRef.*"),
-      ProblemFilters.exclude[DirectMissingMethodProblem]("*mima.core.TemplateProblem.*"),
-      ProblemFilters.exclude[DirectMissingMethodProblem]("*mima.core.MemberProblem.*"),
-      // Changes within the internal parsing code
-      ProblemFilters.exclude[Problem]("*mima.core.BytesReader*"),
-      ProblemFilters.exclude[Problem]("*mima.core.BufferReader*"),
-      ProblemFilters.exclude[Problem]("*mima.core.ClassfileParser*"),
-      ProblemFilters.exclude[Problem]("*mima.core.ClientClassfileParser"),
-      ProblemFilters.exclude[Problem]("*mima.core.LibClassfileParser"),
-      ProblemFilters.exclude[Problem]("*mima.core.UTF8Codec*"),
-      // Dropped dead code internal to types
-      ProblemFilters.exclude[Problem]("*mima.core.Type*"),
-      ProblemFilters.exclude[Problem]("*mima.core.ArrayType*"),
-      ProblemFilters.exclude[Problem]("*mima.core.ClassType*"),
-      ProblemFilters.exclude[Problem]("*mima.core.MethodType*"),
-      ProblemFilters.exclude[Problem]("*mima.core.ValueType*"),
-      // Dropped dead code internal to the info classes
-      ProblemFilters.exclude[Problem]("*mima.core.Definitions*"),
-      ProblemFilters.exclude[Problem]("*mima.core.*Info*"),
-      ProblemFilters.exclude[Problem]("*mima.core.*Members*"),
-      ProblemFilters.exclude[Problem]("*mima.core.No*"),
-      ProblemFilters.exclude[Problem]("*mima.core.Has*"),
-      ProblemFilters.exclude[Problem]("*mima.core.With*"),
-      // Changes in internal, analyze code
-      ProblemFilters.exclude[Problem]("*mima.lib.analyze*"),
-      // Changes in internal code
-      ProblemFilters.exclude[Problem]("*mima.core.Reference*"),
-      ProblemFilters.exclude[Problem]("*mima.core.util.*"),
-      ProblemFilters.exclude[IncompatibleSignatureProblem]("com.typesafe.tools.mima.plugin.MimaPlugin#EmptySet*"),
-      ProblemFilters.exclude[IncompatibleSignatureProblem]("com.typesafe.tools.mima.plugin.MimaPlugin#EmptyMap*"),
     ),
   )
 }
