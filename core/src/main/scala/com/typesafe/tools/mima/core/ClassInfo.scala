dd86f4106cc85d35d7752f45d0892984e1d3ca54
Dale Wijnand <dale.wijnand@gmail.com>
2019-09-27 14:39:12 +0100

    Drop Config.baseClassPath/baseDefinitions (var!)
    That classpath is only used to define ClassInfo.ObjectClass, so:
    
      1. don't make it a var and redefine it
      2. localise its usage

--- core/src/main/scala/com/typesafe/tools/mima/core/ClassInfo.scala
+++ core/src/main/scala/com/typesafe/tools/mima/core/ClassInfo.scala
@@ -8,10 +8,14 @@ import com.typesafe.tools.mima.core.util.log.ConsoleLogging
 object ClassInfo {
   def formatClassName(str: String) = NameTransformer.decode(str).replace('$', '#')
 
-  /** We assume there can be only one java.lang.Object class, and that comes from the configuration
-   *  class path.
+  /** We assume there can be only one java.lang.Object class,
+   *  and that comes from the configuration class path.
    */
-  lazy val ObjectClass = Config.baseDefinitions.ObjectClass
+  lazy val ObjectClass = {
+    val baseClassPath = DeprecatedPathApis.newPathResolver(Config.settings).result
+    val baseDefinitions = new Definitions(None, baseClassPath)
+    baseDefinitions.ObjectClass
+  }
 }
 
 /** A placeholder class info for a class that is not found on the classpath or in a given
