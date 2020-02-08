9c93226de27fa2a4232aea50b6bcd5c7c2477295
Dale Wijnand <dale.wijnand@gmail.com>
2020-02-08 18:16:46 +0000

    Prep for the next Scala release

--- project/TestsPlugin.scala
+++ project/TestsPlugin.scala
@@ -27,6 +27,7 @@ object TestsPlugin extends AutoPlugin {
 
   override def buildSettings = Seq(
     resolvers += "scala-pr-validation-snapshots" at "https://scala-ci.typesafe.com/artifactory/scala-pr-validation-snapshots/",
+    resolvers += "scala-integration" at "https://scala-ci.typesafe.com/artifactory/scala-integration/",
     testScalaVersion := sys.props.getOrElse("mima.testScalaVersion", scalaVersion.value),
   )
 
