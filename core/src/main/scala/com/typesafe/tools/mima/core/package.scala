0d8428fef63aa568aead6c84d7c020783abc4929
Dale Wijnand <dale.wijnand@gmail.com>
2020-02-09 17:39:25 +0000

    Document ProblemFilter

--- core/src/main/scala/com/typesafe/tools/mima/core/package.scala
+++ core/src/main/scala/com/typesafe/tools/mima/core/package.scala
@@ -7,6 +7,7 @@ import scala.tools.nsc.classpath.AggregateClassPath
 import scala.tools.nsc.util.ClassPath
 
 package object core {
+  /** Returns `true` for problems to keep, `false` for problems to drop. */
   type ProblemFilter = Problem => Boolean
 
   private[mima] def pathToClassPath(f: File): Option[ClassPath] =
