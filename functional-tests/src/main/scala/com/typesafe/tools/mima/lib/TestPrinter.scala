fce6138cfe67c57169d47b1b5bf43bb5f77a9124
Dale Wijnand <dale.wijnand@gmail.com>
2020-07-05 17:19:48 +0100

    Make TestPrinter.testAll run all, not fail-fast

--- functional-tests/src/main/scala/com/typesafe/tools/mima/lib/TestPrinter.scala
+++ functional-tests/src/main/scala/com/typesafe/tools/mima/lib/TestPrinter.scala
@@ -6,17 +6,20 @@ object TestPrinter {
   def pass = s"${Console.GREEN}\u2713${Console.RESET}" // check mark (green)
   def fail = s"${Console.RED}\u2717${Console.RESET}"   // cross mark (red)
 
-  def ok(msg: String)                          = Success(println(msg))
-  def ko(msg: String, cause: Throwable = null) = Failure(new Exception(msg, cause))
-
   def testAll[A](xs: Seq[A])(toText: A => String)(test: A => Try[Unit]): Unit = {
-    xs.foldLeft(Try(())) {
-      case (f: Failure[_], _) => f
-      case (_, x)             => test(x) match {
-        case Success(()) => ok(s"+ $pass  ${toText(x)}")
-        case Failure(ex) => ko(s"- $fail  ${toText(x)}", ex)
-      }
+    xs.iterator.map(x => run1(toText(x), () => test(x))).foldLeft(Try(())) {
+      case (Success(()), res)               => res
+      case (res, Success(()))               => res
+      case (res @ Failure(e1), Failure(e2)) => e1.addSuppressed(e2); res
     }.get
+  }
+
+  def run1(label: String, action: () => Try[Unit]): Try[Unit] = {
+    action() match {
+      case res @ Success(()) => println(s"+ $pass  $label"); res
+      case res @ Failure(ex) => println(s"- $fail  $label: $ex"); res
+    }
+  }
 
   if (System.out != Console.out) {
     System.out.println(" System.out identity ##: " + System.identityHashCode(System.out))
