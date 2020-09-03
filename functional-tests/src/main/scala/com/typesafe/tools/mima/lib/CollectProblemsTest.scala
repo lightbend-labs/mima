e475515fcdad70816bea6ce610a9f7eeaae2ce5b
Dale Wijnand <dale.wijnand@gmail.com>
2020-09-03 10:00:22 +0100

    Tweak testing infrastructure code for future changes

--- functional-tests/src/main/scala/com/typesafe/tools/mima/lib/CollectProblemsTest.scala
+++ functional-tests/src/main/scala/com/typesafe/tools/mima/lib/CollectProblemsTest.scala
@@ -3,72 +3,51 @@ package com.typesafe.tools.mima.lib
 import java.io.File
 import java.nio.file.Files
 
-import com.typesafe.config.ConfigFactory
-import com.typesafe.tools.mima.core.{ Problem, ProblemFilters }
+import com.typesafe.tools.mima.core.Problem
 
 import scala.collection.JavaConverters._
 import scala.util.{ Failure, Success, Try }
 
 object CollectProblemsTest {
-  def main(args: Array[String]): Unit = TestCase.argsToTests(args.toList, testCollectProblems).assert()
+  def main(args: Array[String]): Unit = TestCli.argsToTests(args.toList, testCollectProblems).unsafeRunTest()
 
-  def testCollectProblems(testCase: TestCase) = {
-    import testCase._
-    for {
-      () <- compileThem
-      oracleFile = versionedFile(baseDir / "problems.txt")
-      () <- runTest(outV1.jfile, outV2.jfile, baseDir.jfile, oracleFile.jfile)
-    } yield ()
+  def testCollectProblems(testCase: TestCase) = for {
+    () <- testCase.compileThem
+    problems = collectProblems(testCase.outV1.jfile, testCase.outV2.jfile)
+    expected = readOracleFile(testCase.versionedFile(testCase.baseDir / "problems.txt").jfile)
+    () <- diffProblems(problems, expected)
+  } yield ()
+
+  val mimaLib: MiMaLib = new MiMaLib(cp = Nil)
+
+  def collectProblems(v1: File, v2: File): List[Problem] = mimaLib.collectProblems(v1, v2)
+
+  def readOracleFile(oracleFile: File): List[String] = {
+    Files.lines(oracleFile.toPath).iterator.asScala.filter(!_.startsWith("#")).toList
   }
 
-  def runTest(oldJarOrDir: File, newJarOrDir: File, baseDir: File, oracleFile: File): Try[Unit] = {
-    val mima = new MiMaLib(Nil)
-
-    val configFile = new File(baseDir, "test.conf")
-    val configFallback = ConfigFactory.parseString("filter { problems = [] }")
-    val config = ConfigFactory.parseFile(configFile).withFallback(configFallback).resolve()
-    val filters = for (conf <- config.getConfigList("filter.problems").asScala)
-      yield ProblemFilters.exclude(conf.getString("problemName"), conf.getString("matchName"))
-    val problemFilter = (p: Problem) => filters.forall(filter => filter(p))
-
-    val problems = mima.collectProblems(oldJarOrDir, newJarOrDir).filter(problemFilter)
-
-    // load oracle
-    val expectedProblems = Files.lines(oracleFile.toPath).iterator.asScala.filter(!_.startsWith("#")).toList
-
+  def diffProblems(problems: List[Problem], expected: List[String]): Try[Unit] = {
     // diff between the oracle and the collected problems
-    val problemDescriptions = problems.iterator.map(_.description("new")).toSet
-    val unexpectedProblems = problems.filterNot(p => expectedProblems.contains(p.description("new")))
-    val unreportedProblems = expectedProblems.filterNot(problemDescriptions.contains)
+    val unexpected = problems.filter(p => !expected.contains(p.description("new")))
+    val unreported = expected.diff(problems.map(_.description("new")))
 
     val msg = new StringBuilder
-
-    if (unexpectedProblems.nonEmpty) {
-      val qt = """""""
-      unexpectedProblems.iterator
-          .map(_.description("new"))
-          .addString(msg, s"The following ${unexpectedProblems.size} problems were reported but not expected:\n  - ", "\n  - ", "\n")
-      unexpectedProblems.iterator
-          .flatMap(_.howToFilter.map(s => s + ",").toList)
-          .toIndexedSeq
-          .sorted
-          .distinct
-          .addString(msg, "Filter with:\n  ", "\n  ", "\n")
-      unexpectedProblems.iterator
-          .flatMap { p => p.matchName.map(matchName => s"{ matchName=$qt$matchName$qt , problemName=${p.getClass.getSimpleName} }") }
-          .toIndexedSeq
-          .sorted
-          .distinct
-          .addString(msg, "Or filter with:\n  ", "\n  ", "\n")
+    def pp(start: String, lines: List[String]) = {
+      if (lines.isEmpty) ()
+      else lines.sorted.distinct.addString(msg, s"$start (${lines.size}):\n  - ", "\n  - ", "\n")
     }
+    pp("The following problem(s) were expected but not reported", unreported)
+    pp("The following problem(s) were reported but not expected", unexpected.map(_.description("new")))
+    pp("Filter with:", unexpected.flatMap(_.howToFilter))
+    pp("Or filter with:", unexpected.flatMap(p => p.matchName.map { matchName =>
+      s"{ matchName=$dq$matchName$dq , problemName=${p.getClass.getSimpleName} }"
+    }))
 
-    if (unreportedProblems.nonEmpty)
-      //noinspection ScalaUnusedExpression // intellij-scala is wrong, addString _does_ side-effect
-      unreportedProblems.addString(msg, s"The following ${unreportedProblems.size} problems were expected but not reported:\n  - ", "\n  - ", "\n")
-
-    msg.result() match {
+    msg.mkString match {
       case ""  => Success(())
       case msg => Failure(new Exception(msg))
     }
   }
+
+  private final val dq = '"' // scala/bug#6476 -.-
 }
