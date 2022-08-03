b5b4a0e28323a79f62b2cdd6e72c703b9282a5e5
Dale Wijnand <dale.wijnand@gmail.com>
2022-08-03 10:17:50 +0100

    Fix & change tests
    Reclassify package-private-class-becomes as breaking, my bad.
    
    Fix package-private-class-unbecomes, and the fix also correct the
    confusion: it's a backwards binary compatibility test.

---
+++ functional-tests/src/test/package-private-class-becomes-nok/v1/A.scala
@@ -0,0 +1,5 @@
+package foo
+
+class Foo {
+  def bar(x: Int) = x
+}
--- functional-tests/src/test/package-private-class-becomes-nok/v2/A.scala
+++ functional-tests/src/test/package-private-class-becomes-nok/v2/A.scala
@@ -3,8 +3,3 @@ package foo
 private[foo] class Foo {
   def bar(x: Int, y: Int) = x + y
 }
-
-object Lib {
-  val foo  = new Foo
-  def doIt = foo.bar(1, 0)
-}
