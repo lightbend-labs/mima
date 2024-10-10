fd501b2ca9e0d335d2814ee818c051e3e6f812c6
Scala Steward <scala_steward@virtuslab.com>
2024-10-10 18:08:35 +0000

    Update scala3-library to 3.3.4

--- functional-tests/src/main/scala/com/typesafe/tools/mima/lib/TestCli.scala
+++ functional-tests/src/main/scala/com/typesafe/tools/mima/lib/TestCli.scala
@@ -12,7 +12,7 @@ object TestCli {
   val scala211 = "2.11.12"
   val scala212 = "2.12.20"
   val scala213 = "2.13.15"
-  val scala3   = "3.3.3"
+  val scala3   = "3.3.4"
   val hostScalaVersion = StdLibProps.scalaPropOrNone("maven.version.number").get
   val allScalaVersions = List(scala211, scala212, scala213, scala3)
   val testsDir = Directory("functional-tests/src/test")
