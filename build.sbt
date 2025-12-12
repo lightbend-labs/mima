dfdf81b03af7e69b331e26da39d131e0f48167a8
Scala Steward <scala_steward@virtuslab.com>
2025-12-12 18:59:31 +0000

    Update scala-library, scala-reflect to 2.12.21

--- build.sbt
+++ build.sbt
@@ -50,7 +50,7 @@ commands += Command.command("testStaging") { state =>
 }
 
 // Keep in sync with TestCli
-val scala212 = "2.12.20"
+val scala212 = "2.12.21"
 val scala213 = "2.13.18"
 val scala3 = "3.3.6"
 val scala3_7 = "3.7.2"
--- functional-tests/src/main/scala/com/typesafe/tools/mima/lib/TestCli.scala
+++ functional-tests/src/main/scala/com/typesafe/tools/mima/lib/TestCli.scala
@@ -10,7 +10,7 @@ import scala.util.{ Properties => StdLibProps }
 object TestCli {
   // Keep in sync with build.sbt
   val scala211 = "2.11.12"
-  val scala212 = "2.12.20"
+  val scala212 = "2.12.21"
   val scala213 = "2.13.18"
   val scala3   = "3.3.6"
   val hostScalaVersion = StdLibProps.scalaPropOrNone("maven.version.number").get
